ee466fd27ad0e39bd50d3a9a61c4ff7427c99408
Xuantho01 <hoxuantho95@gmail.com>
2020-02-15 11:19:31 +0700

    reverse array

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
 public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array;
+        int size;
+        do{
+            System.out.println("Enter size of arary: ");
+            size = scanner.nextInt();
+            if(size > 20){
+                System.out.println("Size does not exceed 20");
+            }
+        }while(size > 20);
+        array = new int[size];
+        int i = 0;
+        while( i < array.length ){
+            System.out.println("Enter element "+(i + 1)+ ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        for(int j = 0; j < array.length; j++){
+            System.out.println(array[j] +"\t");
+        }
+         for (int j = 0; j < array.length/2; j++){
+             int tmp = array[j];
+             array[j] = array[array.length - j - 1];
+             array[array.length - j - 1] = tmp;
+         }
+         for (int j = 0; j < array.length; j++){
+             System.out.println(array[j]);
+         }
+    }
 }
